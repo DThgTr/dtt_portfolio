e3936259a1dabe60cc9815ff2a5b383b98f8cf14
DThgTr <121519286+DThgTr@users.noreply.github.com>
2024-09-16 14:17:58 -0500

    Add name, title & links to quick info section

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 import About from "./component/About";
 import Resume from "./component/Resume";
 import Contact from "./component/Contact";
+import { Github, Linkedin } from "lucide-react";
 
 export default function Home() {
   const [activeContent, set_activeContent] = useState("About")
@@ -15,7 +16,7 @@ export default function Home() {
           m-[2%] 
           overflow-hidden
     ">
-      {/*CONTENT NAV*/}
+      {/*=====================CONTENT NAV=====================*/}
       <nav className="
             sm:w-1/6 md:w-1/12 
             p-4
@@ -40,9 +41,10 @@ export default function Home() {
         </ul>
       </nav>
 
-      {/* QUICK INTRODUCTION */}
+      {/*=====================QUICK INFO=====================*/}
       <div className="
             flex flex-wrap flex-col
+            items-center
             sm:w-1/3 md:w-3/12 
             rounded-3xl shadow-xl
             p-4 
@@ -57,12 +59,48 @@ export default function Home() {
           />
         </div>
 
-        <div className="break-words overflow-wrap-break-word">
-          Intro text \[T]/
+        <div className="
+                break-words overflow-wrap-break-word
+                flex flex-wrap flex-col
+                justify-center
+                m-[5%]
+                p-[3%]
+        ">
+            <p className="font-bold text-3xl text-center">
+                Duc Thang Tran
+            </p>
+            
+            <div className="my-3 border"></div>
+
+            <p className="font-bold text-base text-center">
+                Software Engineer
+            </p>
+
+            <div className="my-3"></div>
+            
+            <div className="
+                flex flex-wrap flex-row
+                justify-center
+            ">
+              <a href="https://www.linkedin.com/in/ducthangtran" target="_blank"
+                className="
+                  hover:opacity-50
+                  p-2
+              ">
+                <Linkedin className="w-8 h-8"/>
+              </a>
+              <a href="https://github.com/DThgTr" target="_blank"
+                className="
+                  hover:opacity-50
+                  p-2
+              ">
+                <Github className="w-8 h-8"/>
+              </a>
+            </div>
         </div>
       </div>
       
-      {/* CONTENT */}
+      {/*=====================CONTENT=====================*/}
       <div className="
             flex-grow 
             p-4 
@@ -70,15 +108,21 @@ export default function Home() {
             rounded-3xl shadow-lg
             bg-blue-800 bg-opacity-15
       ">
-        {
-          activeContent === "About" && <About/>
-        }
-        {
-          activeContent === "Resume" && <Resume/>
-        }
-        {
-          activeContent === "Contact" && <Contact/>
-        }
+        <div className="
+              p-4
+              size-[100%]
+              border rounded-3xl border-white
+        ">
+          {
+            activeContent === "About" && <About/>
+          }
+          {
+            activeContent === "Resume" && <Resume/>
+          }
+          {
+            activeContent === "Contact" && <Contact/>
+          }
+        </div>
       </div>
     </div>
   )
