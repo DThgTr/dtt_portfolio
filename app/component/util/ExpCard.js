a9e6292b80db979399690cd118271886583ceb1a
DThgTr <121519286+DThgTr@users.noreply.github.com>
2024-09-26 21:02:25 -0500

    add responsive design (again) (pls do not follow my example)

--- app/component/util/ExpCard.js
+++ app/component/util/ExpCard.js
@@ -5,7 +5,6 @@ export default function ExpCard({position, company,  date, content, skills}) {
             rounded-lg 
             shadow-slate-800 shadow-md
             my-3
-            overflow-hidden 
             w-full
         ">
             <div className="p-[5%]">
@@ -13,7 +12,7 @@ export default function ExpCard({position, company,  date, content, skills}) {
                 <p className="font-bold text-xl pb-2 mb-2 border-b">{position}</p>
                 <p className="text-sm text-gray-400">{company}</p>
                 <p className="text-base my-3">{content}</p>
-                <div className="flex flex-row">
+                <div className="flex overflow-auto md:flex-row">
                     {skills.map((skill) => {
                         return(
                             <p className="bg-blue-600 border rounded-xl px-2 py-1 m-1 hover:brightness-150 text-base">{skill}</p>
--- app/component/util/ProjCard.js
+++ app/component/util/ProjCard.js
@@ -4,15 +4,14 @@ export default function ProjCard({proj_name, proj_link, description, skills}) {
             bg-indigo-800
             rounded-lg 
             shadow-slate-800 shadow-md
-            my-3
-            overflow-hidden 
+            my-3 
             w-full
         ">
             <div className="p-[5%]">
                 <p className="font-bold text-xl pb-2 mb-2 border-b">{proj_name}</p>
                 <a href={proj_link[1]} className="hover:text-blue-300 hover:opacity-50 underline text-sm text-gray-400">{proj_link[0]}</a>
                 <p className="text-base my-3">{description}</p>
-                <div className="flex flex-row">
+                <div className="flex overflow-auto md:flex-row">
                     {skills.map((skill) => {
                         return(
                             <p className="bg-blue-600 border rounded-xl px-2 py-1 m-1 hover:brightness-150 text-base">{skill}</p>
